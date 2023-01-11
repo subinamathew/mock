41bb9d4aaadf5c4008014dd2261e7cdec05c53e3
subinamathew <smathew2237@gmail.com>
2023-01-11 08:42:12 +0000

    e

--- src/main/java/com/services/spring/controller/MyRestController.java
+++ src/main/java/com/services/spring/controller/MyRestController.java
@@ -7,9 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyRestController {
 
-    @RequestMapping(value = "/tt")
+    @RequestMapping(value = "/")
     public String test() {
-        return "jhjj";
+        return "End Points \n ";
     }
+
+
+    public String getJWTToken() {
+        MyJWTService myservice = new MyJWTService();
+        WebClient client = WebClient.create();
+        //WebClient.ResponseSpec responseSpec = client.get()
+        //    .uri("http://example.com")
+        //    .retrieve();
+       // return responseSpec.bodyToMono(String.class).block();
+       return myservice.getJWTSource();
+    }
+
 
 }
---
+++ src/main/java/com/services/spring/services/MyJWTService.java
@@ -0,0 +1,8 @@
+package com.services.spring.services;
+
+public class MyJWTService {
+
+    public String getJWTSource() {
+        return "https://httpbin.org/get";
+    }
+}
