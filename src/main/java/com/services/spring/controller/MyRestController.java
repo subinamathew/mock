d4909c4fefe540a875febca9dfd17dbfbae87b98
subinamathew <smathew2237@gmail.com>
2023-01-13 11:57:29 +0000

    mid

--- src/main/java/com/services/spring/controller/MyRestController.java
+++ src/main/java/com/services/spring/controller/MyRestController.java
@@ -1,16 +1,15 @@
 package com.services.spring.controller;
 
-import java.io.UnsupportedEncodingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import com.services.spring.services.ConjurService;
 import com.services.spring.services.GCPIdentityService;
@@ -32,10 +31,12 @@ public class MyRestController {
         RestTemplate restTemplate = new RestTemplate();
         String JWTToken = "";
         try {
+            String body = gcpIdentityService.getGCPServiceData();
             HttpHeaders headers = new HttpHeaders();
             headers.set(gcpIdentityService.getGCPMetadataKey(), gcpIdentityService.getGCPMetadataValue());
-            HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-            String requestURL = gcpIdentityService.GCPRequestURL(conjurService.getConjurGcpAudience());
+            HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
+            String gcpServiceIdentity = gcpIdentityService.getGCPServiceIdentity();
+            String requestURL = gcpIdentityService.getGCPRequestURL(conjurService.getConjurGcpAudience(gcpServiceIdentity));
             ResponseEntity<String> response = restTemplate.exchange(requestURL,HttpMethod.GET, requestEntity, String.class);
             JWTToken = response.getBody();
         } catch (Exception  e) {
@@ -51,13 +52,12 @@ public class MyRestController {
      * This is used to get the Conjur Authetication Token
      * @return Conjur Authentication Token. This is valid for 8-10 minutes normally.
      */
-    @RequestMapping(value ="/conjur")
-    public String getConjurURL(String JWTToken) {
+    @RequestMapping(value ="/conjur/{JWTToken}")
+    public String getConjurToken(@PathVariable String JWTToken) {
         HttpHeaders headers = new HttpHeaders();
-        headers.set(gcpIdentityService.getGCPMetadataKey(), gcpIdentityService.getGCPMetadataValue());
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         //header accepts base64
-        HttpEntity<String> requestEntity = new HttpEntity<String>(JWTToken,headers);
+        HttpEntity<String> requestEntity = new HttpEntity<>(conjurService.getFormatedJWT(JWTToken),headers);
         RestTemplate restTemplate = new RestTemplate();
         String conjurAuthToken = "";
         String conjurUrl = conjurService.getConjurURL();
@@ -68,8 +68,27 @@ public class MyRestController {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        return conjurAuthToken;
+    }
 
-        conjurAuthToken = conjurUrl; //DELETE ME
+    @RequestMapping(value ="/accessSecret")
+    public String getSecret(String conjurToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(gcpIdentityService.getGCPMetadataKey(), gcpIdentityService.getGCPMetadataValue());
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        //header accepts base64
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(conjurToken,headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String conjurAuthToken = "";
+        String conjurUrl = conjurService.getConjurURL();
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(conjurUrl,requestEntity, String.class);
+            conjurAuthToken = response.getBody();
+        } catch (Exception  e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return conjurAuthToken;
     }
 
